df976a1228f5870e635c718cdb3d8dbef0279452
Augusto Bernardes <augustobdev@outlook.com>
2025-06-25 14:20:32 -0300

    feat: setting up express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import express from 'express'
 
-function test(){
-    console.log("test")
-}
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`)
+})
