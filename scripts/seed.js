447862119b544cb5819337f705f24b36b882582e
Augusto Bernardes <augustobdev@outlook.com>
2025-06-24 21:15:57 -0300

    fix: updating seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,9 +5,25 @@ const prisma = new PrismaClient();
 async function seed() {
   await prisma.months.createMany({
     data: [
-      { month: new Date(2025, 0, 1), id: '1' },
-      { month: new Date(2025, 1, 1), id: '2' },
-      { month: new Date(2025, 2, 1), id: '3' },
+      { month: '2025-01', id: '1' },
+      { month: '2025-02', id: '2' },
+      { month: '2025-03', id: '3' },
+    ],
+  });
+
+  await prisma.clients.createMany({
+    data: [
+      { id: '1', name:'Augusto', phone: '649910', cpf:'022', birth_date: new Date(2004, 2, 30), current_invoice_price: 100.2 },
+      { id: '2', name:'Otavio', phone: '1233', cpf:'123', birth_date: new Date(2007, 3, 3), current_invoice_price: 30 },
+    ],
+  });
+
+  await prisma.invoices.createMany({
+    data: [
+      { id: '1',status: 'paid', month_id: '1', client_id: '1', value: 80 },
+      { id: '2',status: 'paid', month_id: '2', client_id: '1', value: 90 },
+      { id: '3',status: 'paid', month_id: '3', client_id: '1', value: 100.2 },
+      { id: '4', status: 'paid', month_id: '3', client_id: '2', value: 30 },
     ],
   });
 }
